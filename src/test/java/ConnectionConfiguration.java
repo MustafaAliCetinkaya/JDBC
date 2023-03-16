426043301e619fa98424e8cffa7f401c0dd71d7f
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-16 18:50:02 +0300

    JDBC query is done

--- src/test/java/ConnectionConfiguration.java
+++ src/test/java/ConnectionConfiguration.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
---
+++ src/test/java/DatabaseMetaData.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseMetaData {
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:oracle:thin:@100.25.202.239:1521:XE";
+        String username = "hr";
+        String password = "hr";
+
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+        java.sql.DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+        System.out.println("User: "+databaseMetaData.getUserName());
+        System.out.println("Product name: "+databaseMetaData.getDatabaseProductName());
+        System.out.println("Product version: "+databaseMetaData.getDatabaseProductVersion());
+        System.out.println("Driver name: "+databaseMetaData.getDriverName());
+        System.out.println("Driver version: "+databaseMetaData.getDriverVersion());
+    }
+}
