023339ba3a275e5a0897faa9623f8164add085bc
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-16 09:32:26 +0300

    JDBC Connection is done

--- src/test/java/ConnectionConfiguration.java
+++ src/test/java/ConnectionConfiguration.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class ConnectionConfiguration {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionConfiguration {
+    public static void main(String[] args) {
+
+        String url="jdbc:oracle:thin:@100.25.202.239:1521:XE";
+        String username="hr";
+        String password="hr";
+
+        try {
+            Connection connection= DriverManager.getConnection(url, username, password);
+            System.out.println("Connection was successful");
+        } catch (SQLException e) {
+            System.out.println("Connection has failed! "+ e.getMessage());
+        }
+    }
 }
