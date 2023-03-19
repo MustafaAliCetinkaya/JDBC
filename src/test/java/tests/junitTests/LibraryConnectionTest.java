edc2fd2575052e2fd97f0b115a43499f3c1eadf5
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-03-19 18:03:25 +0300

    JDBC query is done successfully

---
+++ src/test/java/tests/junitTests/LibraryConnectionTest.java
@@ -0,0 +1,58 @@
+package tests.junitTests;
+
+import org.junit.jupiter.api.Test;
+import utilities.DBUtils;
+
+import java.sql.*;
+
+public class LibraryConnectionTest {
+
+    @Test
+    public void test1() throws SQLException {
+        String dbUrl = "jdbc:mysql://34.230.35.214:3306/library1";
+        String dbUsername = "library1_client";
+        String dbPassword = "WVF4NdGXCKHeE6VQ";
+
+        Connection connection = DriverManager.getConnection(dbUrl,dbUsername,dbPassword);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM books");
+
+        //once you set up connection default pointer looks for 0
+        //next() --> move pointer to first row
+        resultSet.next();
+        System.out.println(resultSet.getString(2));
+
+        ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+
+        for (int i = 1; i < resultSetMetaData.getColumnCount(); i++) {
+            System.out.println(resultSetMetaData.getColumnName(i));
+        }
+
+        while (resultSet.next()){
+            for (int i = 1; i < resultSetMetaData.getColumnCount(); i++) {
+                System.out.print(resultSet.getString(i)+"\t");
+            }
+            System.out.println();
+        }
+
+
+        //close connection
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+
+
+    }
+
+    @Test
+    public void test2(){
+        String dbUrl = "jdbc:mysql://34.230.35.214:3306/library1";
+        String dbUsername = "library1_client";
+        String dbPassword = "WVF4NdGXCKHeE6VQ";
+
+        DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
+
+        DBUtils.destroy();
+    }
+}
